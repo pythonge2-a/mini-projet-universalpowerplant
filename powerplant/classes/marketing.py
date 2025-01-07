7644054b548d9792053c1cbf0e37cd3259005d8f
porretleo <leo.porret@heig-vd.ch>
2025-01-07 09:07:44 +0100

    modif marketing

--- powerplant/classes/marketing.py
+++ powerplant/classes/marketing.py
@@ -4,8 +4,8 @@
 
 class marketing:
     def __init__(self):
-        self.user_price = 0
-        self.bank = 0
+        self.user_price = 0 #pas utilisé 
+        self.bank = 0 #pas utilisé
 
     def get_user_gain(self, selling_price, unit_sold):    #retourne les gains utilisateur par seconde
         return selling_price * unit_sold
